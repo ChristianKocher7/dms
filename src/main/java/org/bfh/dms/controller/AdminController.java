fcee1ba963e619440454ba53ce1fc4f2babe41ab
Christian Kocher <christian.kocher@mimacom.com>
2019-10-21 11:05:05 +0200

    changed endpoint name

--- src/main/java/org/bfh/dms/controller/AdminController.java
+++ src/main/java/org/bfh/dms/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/admin")
+@RequestMapping(value = "/api")
 public class AdminController {
 
     private DeviceService deviceService;
