72090f33505afeb5de293f269e54272e1421bb17
Christian Kocher <christian.kocher@mimacom.com>
2019-11-22 10:06:58 +0100

    updated datamodel and added mock factory

--- src/main/java/org/bfh/dms/service/DeviceService.java
+++ src/main/java/org/bfh/dms/service/DeviceService.java
@@ -3,11 +3,11 @@ package org.bfh.dms.service;
 import org.bfh.dms.domain.Device;
 import org.bfh.dms.dto.DeviceDto;
 import org.bfh.dms.mapper.DtoMapper;
+import org.bfh.dms.mock.MockFactory;
 import org.bfh.dms.repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,20 +17,20 @@ public class DeviceService {
     @Autowired
     private DeviceRepository deviceRepository;
 
-    public List<DeviceDto> getAllDevices(){
+    public List<DeviceDto> getAllDevices() {
         System.out.println("saving new device to db....");
-        saveDevice(DeviceDto.of("X203984", "Lenovo", "John Doe", LocalDate.now()));
+        saveDevice(MockFactory.createDeviceDto());
         System.out.println("device saved");
         List<Device> devices = deviceRepository.findAll();
         List<DeviceDto> deviceDtos = new ArrayList<>();
         System.out.println("mapping dtos...");
-        for(Device device : devices){
+        for (Device device : devices) {
             deviceDtos.add(DtoMapper.map(device));
         }
         return deviceDtos;
     }
 
-    public void saveDevice(DeviceDto deviceDto){
+    public void saveDevice(DeviceDto deviceDto) {
         deviceRepository.save(DtoMapper.map(deviceDto));
     }
 }
